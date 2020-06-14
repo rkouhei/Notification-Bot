cc94b04e73a5e661bcb3c12fe74973463264dbb7
RKouhei <kohriki222@gmail.com>
2020-06-14 12:11:43 +0900

    made scheduleTask and made it possible to call it from index

--- functions/src/checkUrl.js
+++ functions/src/checkUrl.js
@@ -1,19 +1,20 @@
 'use strict';
 
-const express = require('express');
-const firebase = require('firebase-admin');
+// const express = require('express');
+// const firebase = require('firebase-admin');
 const sitejson = require('../data/site.json');
 const request = require('request-promise');
 const cheerio = require('cheerio');
+//const index = require('../index');
 
-const serviceAccount = require('../../env/firebase-project.json');
-const dbUrl = require('../../env/db-url.json');
-firebase.initializeApp({
-  credential: firebase.credential.cert(serviceAccount),
-  databaseURL: dbUrl.url
-});
+// const serviceAccount = require('../../env/firebase-project.json');
+// const dbUrl = require('../../env/db-url.json');
+// firebase.initializeApp({
+//   credential: firebase.credential.cert(serviceAccount),
+//   databaseURL: dbUrl.url
+// });
 
-const app = express();
+//const app = express();
 
 async function init() {
   // bodyとってくる
@@ -32,7 +33,8 @@ async function init() {
         const textBody = $(urls.parts).text();
         const d = {
           url: urls.url,
-          body: textBody
+          body: textBody,
+          parts: urls.parts
         }
         data.push(d);
       } catch (e) {
@@ -45,33 +47,99 @@ async function init() {
   // url作る(日毎に異なるやつ)
   for ( let i in data ) {
     //console.log(data[i]);
-    //await setHtml(data[i]);
+    await setHtml(data[i], this.db);
   }
 }
 
+async function scheduleTask() {
+  const data = [];
+  let allHtml = await getAllHtml(this.db);
+
+  await Promise.all(allHtml.map(async function(item) {
+    if (item.url !== 'dummy') {
+      await request(item.url, (e, response, body) => {
+        if (e) {
+          console.error(e)
+        }
+        try {
+          const $ = cheerio.load(body)
+          const newTextBody = $(item.parts).text();
+          console.log('今からbody比較する URL:'+item.url)
+          if ( item.body !== newTextBody ) {
+            console.log('body比較したけど違った')
+            const d = {
+              id: item.id,
+              url: item.url,
+              body: newTextBody,
+              parts: item.parts
+            }
+            data.push(d);
+          }
+        } catch (e) {
+          console.error(e)
+        }
+      })
+    }
+  }))
+
+  console.log('set前')
+  console.log(data);
+  for ( let i in data ) {
+    console.log('入ってる?')
+    await updateHtmlById(data[i], this.db)
+    // push 関数
+    // await index.doPushMessage(data[i].url)
+  }
+  return data
+
+  // exports.{{Function名}} = functions.pubsub.schedule('every 1 minutes').onRun((context) => {
+  //   console.log("毎分実行します！");
+  //   return null;
+  // });
+}
+
 async function finish() {
-  let allHtml = await getAllHtml();
+  const db_finish = this.db;
+  let allHtml = await getAllHtml(db_finish);
     
   allHtml.map(function(item) {
     if (item.url !== 'dummy') {
-      deleteHtmlById(item.id);
+      deleteHtmlById(item.id, db_finish);
     }
   });
 }
 
-// init()
+//init()
 //getAllHtml();
-finish()
+//finish()
+//scheduleTask()
 
 //-- crud --//
-async function setHtml(data) {
-  let db = firebase.database();
+// tmp function
+let db;
+function setDB(firebase) {
+  this.db = firebase.database();
+}
+
+async function setHtml(data, db) {
+  //let db = firebase.database();
   let lastId = await getLastHTMLId(db);
   lastId++;
   db.ref('HTML/' + lastId).set({
     body: data.body,
     id: lastId,
-    url: data.url
+    url: data.url,
+    parts: data.parts
+  });
+}
+
+async function updateHtmlById(data, db) {
+  //let db = firebase.database();
+  db.ref('HTML/' + data.id).set({
+    body: data.body,
+    id: data.id,
+    url: data.url,
+    parts: data.parts
   });
 }
 
@@ -93,13 +161,13 @@ async function getLastHTMLId(db) {
   return lastHTMLId;
 }
 
-async function deleteHtmlById(id) {
-  let db = firebase.database();
+async function deleteHtmlById(id, db) {
+  //let db = firebase.database();
   db.ref('HTML/' + id).set({});
 }
 
-async function getAllHtml() {
-  let db = firebase.database();
+async function getAllHtml(db) {
+  //let db = firebase.database();
   let allHtml;
 
   await db.ref("HTML")
@@ -119,3 +187,9 @@ async function getAllHtml() {
 }
 
 //-- end crud --//
+module.exports = {
+  setDB: setDB,
+  scheduleTask: scheduleTask,
+  init: init,
+  finish: finish
+}
